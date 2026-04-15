571c4bc127ad3cfcafea24fcffe47d432181da88
Luis Parrado <luisprmat@gmail.com>
2026-04-15 16:10:05 -0500

    Style guide to Tailwind Conversion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Frontend Mentor | Product list with cart</title>
+
+        {{-- Fonts --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <h1>Frontend Mentor Challenge With Laravel</h1>
+        <div class="flex gap-4 p-8">
+            <div class="size-24 rounded-lg bg-red"></div>
+            <div class="size-24 rounded-lg bg-green"></div>
+            <div class="size-24 rounded-lg bg-rose-50"></div>
+            <div class="size-24 rounded-lg bg-rose-100"></div>
+            <div class="size-24 rounded-lg bg-rose-300"></div>
+            <div class="size-24 rounded-lg bg-rose-400"></div>
+            <div class="size-24 rounded-lg bg-rose-500"></div>
+            <div class="size-24 rounded-lg bg-rose-900"></div>
+        </div>
+        <p class="text-4xl font-bold">Probando la nueva fuente.</p>
     </body>
 </html>
