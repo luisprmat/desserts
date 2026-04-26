ed1f26c55834341db59fe3d18159beed0938eb38
Luis Parrado <luisprmat@gmail.com>
2026-04-26 12:39:51 -0500

    Make cart sticky

--- resources/views/components/cart/index.blade.php
+++ resources/views/components/cart/index.blade.php
@@ -1,7 +1,7 @@
 @props (['cart'])
 
 <aside>
-    <div class="rounded-xl bg-white p-6">
+    <div class="sticky top-2 rounded-xl bg-white p-6">
         <h2 class="text-red text-2xl font-bold">{{ __('Your Cart') }} ({{ $cart?->totalItemsCount() ?? 0 }})</h2>
         @if ($cart?->totalItemsCount())
             <x-cart.active :$cart />
