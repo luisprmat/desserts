b5dc0dfa5f8b3b3963d600805e2dce6b3bc8b01f
Luis Parrado <luisprmat@gmail.com>
2026-04-26 13:29:24 -0500

    Add favicon Frontend Mentors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,11 @@
         rel="stylesheet"
     />
 
+    <link rel="icon" href="/favicon/favicon-16x16.png" sizes="16x16" type="image/png" />
+    <link rel="icon" href="/favicon/favicon-32x32.png" sizes="32x32" type="image/png" />
+    <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" sizes="180x180" type="image/png" />
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" type="image/svg" />
+
     @vite (['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
